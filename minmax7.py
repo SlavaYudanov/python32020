7209b72012063d4071084d8e8231dd2f062c7bcb
SlavaYudanov <73465632+SlavaYudanov@users.noreply.github.com>
2020-12-28 17:33:20 +0300

    Update minmax7.py

--- minmax7.py
+++ minmax7.py
@@ -3,16 +3,16 @@ a = []
 imn = 0
 imx = 0
 for i in range(n):
-a.append(int(input()))
+  a.append(int(input()))
 mn = a[0]
 mx = a[0]
 for i in range (n):
-if a[i]<=mn:
-mn = a[i]
-imn = i
-if a[i]>mx:
-mx = a[i]
-imx = i
+  if a[i]<=mn:
+    mn = a[i]
+    imn = i
+  if a[i]>mx:
+    mx = a[i]
+    imx = i
 print(mn,imn)
 print(mx,imx)
 
