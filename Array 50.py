50b1c2d26b68ff654b55c59daa8428add9d9307a
SlavaYudanov <73465632+SlavaYudanov@users.noreply.github.com>
2020-12-28 17:29:04 +0300

    Update Array 50.py

--- Array 50.py
+++ Array 50.py
@@ -1,12 +1,12 @@
 N = int(input())
 a = []
 for i in range(N):
-a.append(int(input()))
-n_inverse = 0
+  a.append(int(input()))
+  n_inverse = 0
 for i in range(0,N-1) :
-for j in range(i+1,N) :
-if a[i] > a[j] :
-n_inverse += 1
+  for j in range(i+1,N) :
+    if a[i] > a[j] :
+      n_inverse += 1
 print("колл-во инверсий:", n_inverse)
 
 
