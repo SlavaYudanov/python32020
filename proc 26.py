ac81f19e9c6602c1e86573b80fc1917ce038256e
SlavaYudanov <73465632+SlavaYudanov@users.noreply.github.com>
2020-12-28 17:34:21 +0300

    Update proc 26.py

--- proc 26.py
+++ proc 26.py
@@ -1,11 +1,11 @@
 def IsPower5(K):
-Res = 5
-while Res < K:
-Res = Res * 5
-if Res == K:
-return 'true'
-else:
-return 'false'
+  Res = 5
+  while Res < K:
+  Res = Res * 5
+  if Res == K:
+  return 'true'
+  else:
+  return 'false'
 
 print(IsPower5(125))
 print(IsPower5(100))
