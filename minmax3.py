6255c326ba97450ca3e7ad24fd09a036bed5077b
SlavaYudanov <73465632+SlavaYudanov@users.noreply.github.com>
2020-12-28 17:32:28 +0300

    Update minmax3.py

--- minmax3.py
+++ minmax3.py
@@ -1,13 +1,13 @@
 N = int(input("Введите количество прямоугольников"))
 m = 0
 for i in range (N):
-a = int(input("Введите стороны прямоугольника"))
-b = int(input())
-P = 2*(a+b)
-if (i==1):
-m = P
-if (P > m):
-m = P
+  a = int(input("Введите стороны прямоугольника"))
+  b = int(input())
+  P = 2*(a+b)
+  if (i==1):
+    m = P
+  if (P > m):
+    m = P
 print(m)
 
 
