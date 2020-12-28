98f566527ff6e4ffa33eda8e166cf9b23830a758
SlavaYudanov <73465632+SlavaYudanov@users.noreply.github.com>
2020-12-28 17:28:22 +0300

    Update matrix 35.py

--- matrix 35.py
+++ matrix 35.py
@@ -6,28 +6,29 @@ k = 0
 n = -1
 
 for i in range(0, M):
-for i in range(0, N):
-A.append(int(input()))
-MAT.append(A)
-A = []
+  for i in range(0, N):
+    A.append(int(input()))
+    MAT.append(A)
+    A = []
 
 for i in range(M):
-print(MAT[i])
+  print(MAT[i])
 
 for i in range(0, N):
-for j in range(0, M):
-if MAT[j][i] % 2 != 0:
-k = 1
-else:
-k = 0
-break
-if k == 1:
-n = i
-break
+  for j in range(0, M):
+    if MAT[j][i] % 2 != 0:
+      k = 1
+    else:
+      k = 0
+      break
+  if k == 1:
+    n = i
+    break
+    
 if n == -1:
-print("таких столбиков нет")
+  print("таких столбиков нет")
 else:
-print(n)
+  print(n)
 
 
 
